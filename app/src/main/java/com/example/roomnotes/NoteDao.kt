ac9883ba682e8d1b0cb428eee0128f7a9d8e0c4b
adewansyh1608 <2311523001_andre@student.unand.ac.id>
2025-12-03 09:38:39 +0700

    update baru

---
+++ app/src/main/java/com/example/roomnotes/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.example.roomnotes
+
+import androidx.room.*
+import com.example.roomnotes.data.Note
+import kotlinx.coroutines.flow.Flow
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    fun getAllNotes(): Flow<List<Note>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note): Long
+    @Update
+    suspend fun update(note: Note)
+    @Delete
+    suspend fun delete(note: Note)
+    @Query("SELECT * FROM notes WHERE id = :id LIMIT 1")
+    suspend fun getById(id: Long): Note?
+}
---
+++ app/src/main/java/com/example/roomnotes/data/AppDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.roomnotes.data
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomnotes.NoteDao
+
+@Database(entities = [Note::class], version = 1, exportSchema =
+    false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                val inst = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = inst
+                inst
+            }
+    }
+}
